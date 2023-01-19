415baf0d1a5602f00ca0f9477c5af2e263fe353d
Abdullah_Sheikh <h.abdullahsheikh568@gmail.com>
2023-01-19 13:29:45 +0500

    assignment

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-3xl text-green-600 p-2 text-center">
+        Hello World Next.js App with Tailwind CSS!
+      </h1>
+    </div>   
+  )
+}
